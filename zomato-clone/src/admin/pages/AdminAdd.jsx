935f53b7d6f2ccb38a2e273aade92906d65e5ce4
KarthikUllal <karthik2003ku@email.com>
2026-03-06 14:01:28 +0530

    implemented add food and restaurant functionality

--- zomato-clone/src/admin/pages/AdminAdd.jsx
+++ zomato-clone/src/admin/pages/AdminAdd.jsx
@@ -1,8 +1,141 @@
 import { useState } from "react";
 import "../styles/AdminAdd.css";
+import axios from "axios";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function AdminAdd() {
   const [activeTab, setActiveTab] = useState("restaurant");
+
+  const [restaurants, setRestaurants] = useState([])
+
+  const [restaurantData, setRestaurantData] = useState({
+    name: "",
+    category: "dining",
+    cuisine: "",
+    location: "",
+    averageCostForTwo: "",
+    description: "",
+    hours: "",
+    contact: "",
+    banner: null,
+    gallery: [],
+  });
+
+  const [foodData, setFoodData] = useState({
+    name: "",
+    price: "",
+    description: "",
+    foodCategory: "",
+    isVeg: true,
+    restaurant: "",
+    image: null,
+  });
+
+  const handleRestaurantChange = (e) => {
+    const { name, value } = e.target;
+
+    setRestaurantData({
+      ...restaurantData,
+      [name]: value,
+    });
+  };
+
+  const handleBanner = (e) => {
+    setRestaurantData({
+      ...restaurantData,
+      banner: e.target.files[0],
+    });
+  };
+
+  const handleGallery = (e) => {
+    setRestaurantData({
+      ...restaurantData,
+      gallery: e.target.files,
+    });
+  };
+
+  //to fetch restaurant so that it can show dropdown with restuarant name in food add form
+  useEffect(() =>{
+    axios.get("http://localhost:8000/api/admin/restaurants").then(
+      (res) =>{
+        if(res.data.status == "SUCCESS"){
+          setRestaurants(res.data.restaurants);
+        }
+      }
+    ).catch((err) =>{
+      console.log(err);
+    })
+  }, [])
+
+  const submitRestaurant = async () => {
+
+  const formData = new FormData();
+
+  for (let key in restaurantData) {
+
+    if (key === "gallery") {
+
+      for (let i = 0; i < restaurantData.gallery.length; i++) {
+        formData.append("gallery", restaurantData.gallery[i]);
+      }
+
+    } else {
+
+      formData.append(key, restaurantData[key]);
+
+    }
+
+  }
+
+  try {
+
+    const res = await axios.post(
+      "http://localhost:8000/api/admin/restaurant",
+      formData
+    );
+
+    toast.success(res.data.message || "Restaurant added");
+
+  } catch (error) {
+
+    console.log(error);
+    toast.error("Error adding restaurant");
+
+  }
+
+};
+
+  const handleFoodChange = (e) => {
+    const { name, value } = e.target;
+
+    setFoodData({
+      ...foodData,
+      [name]: value,
+    });
+  };
+
+  const handleFoodImage = (e) => {
+    setFoodData({
+      ...foodData,
+      image: e.target.files[0],
+    });
+  };
+
+  const submitFood = async () => {
+    const formData = new FormData();
+
+    for (let key in foodData) {
+      formData.append(key, foodData[key]);
+    }
+
+    await fetch("http://localhost:8000/api/admin/food", {
+      method: "POST",
+      body: formData,
+    });
+
+    alert("Food added");
+  };
 
   return (
     <div className="admin-add-wrapper">
@@ -28,25 +161,63 @@ export default function AdminAdd() {
         <div className="form-box">
           <h2>Add Restaurant</h2>
 
-          <input placeholder="Restaurant Name" />
+          <input
+            name="name"
+            placeholder="Restaurant Name"
+            onChange={handleRestaurantChange}
+          />
 
-          <select>
-            <option>Dining</option>
-            <option>Delivery</option>
-            <option>Nightlife</option>
+          <select name="category" onChange={handleRestaurantChange}>
+            <option value="dining">Dining</option>
+            <option value="delivery">Delivery</option>
+            <option value="nightlife">Nightlife</option>
           </select>
 
-          <input placeholder="Cuisine" />
-          <input placeholder="Location" />
-          <input placeholder="Average Cost For Two (₹1200 for two)" />
-          {/* <input placeholder="Banner Image URL" type="" /> */}
-          <input type="file" id="image" name="image" value=""  />
-          <textarea placeholder="Gallery Image URLs (one per line)"  rows={6}></textarea>
-          <input placeholder="Opening Hours (10 AM - 11 PM)" />
-          <input placeholder="Contact Number" />
-          <textarea placeholder="Description" rows={6}></textarea>
+          <input
+            name="cuisine"
+            placeholder="Cuisine"
+            onChange={handleRestaurantChange}
+          />
 
-          <button className="submit-btn">Add Restaurant</button>
+          <input
+            name="location"
+            placeholder="Location"
+            onChange={handleRestaurantChange}
+          />
+
+          <input
+            name="averageCostForTwo"
+            placeholder="Average Cost For Two"
+            onChange={handleRestaurantChange}
+          />
+
+          <label>Banner Image</label>
+          <input type="file" onChange={handleBanner} />
+
+          <label>Gallery Images</label>
+          <input type="file" multiple onChange={handleGallery} />
+
+          <input
+            name="hours"
+            placeholder="Opening Hours"
+            onChange={handleRestaurantChange}
+          />
+
+          <input
+            name="contact"
+            placeholder="Contact Number"
+            onChange={handleRestaurantChange}
+          />
+
+          <textarea
+            name="description"
+            placeholder="Description"
+            onChange={handleRestaurantChange}
+          ></textarea>
+
+          <button className="submit-btn" onClick={submitRestaurant}>
+            Add Restaurant
+          </button>
         </div>
       )}
 
@@ -54,20 +225,55 @@ export default function AdminAdd() {
         <div className="form-box">
           <h2>Add Food</h2>
 
-          <input placeholder="Food Name" />
-          <input type="number" placeholder="Price" />
-          <input type="file" placeholder="Food Image URL" />
-          <input placeholder="Food Category (Biryani, Pizza etc.)" />
+          <input
+            name="name"
+            placeholder="Food Name"
+            onChange={handleFoodChange}
+          />
 
-          <select>
-            <option>Veg</option>
-            <option>Non-Veg</option>
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            onChange={handleFoodChange}
+          />
+
+          <input type="file" onChange={handleFoodImage} />
+
+          <input
+            name="foodCategory"
+            placeholder="Food Category"
+            onChange={handleFoodChange}
+          />
+
+          <select name="isVeg" onChange={handleFoodChange}>
+            <option value="true">Veg</option>
+            <option value="false">Non-Veg</option>
           </select>
 
-          <input placeholder="Restaurant ID" />
-          <textarea placeholder="Food Description"></textarea>
+          {/* <input
+            name="restaurant"
+            placeholder="Restaurant ID"
+            onChange={handleFoodChange}
+          /> */}
+          <select name="restaurant" onChange={handleFoodChange}>
+            <option value="">Select Restaurant</option>
+            {restaurants.map((restaurant) => (
+              <option key={restaurant._id} value={restaurant._id}>
+                {restaurant.name}
+              </option>
+            ))}
+          </select>
 
-          <button className="submit-btn">Add Food</button>
+          <textarea
+            name="description"
+            placeholder="Food Description"
+            onChange={handleFoodChange}
+          ></textarea>
+
+          <button className="submit-btn" onClick={submitFood}>
+            Add Food
+          </button>
         </div>
       )}
     </div>
