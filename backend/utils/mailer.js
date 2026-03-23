413035a41ec5ccb4e37f99184b4be9c23c8b58c0
KarthikUllal <karthik2003ku@email.com>
2026-03-23 10:55:22 +0530

    testing smpt issue

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,17 +1,36 @@
-//utils/mailer.js
+// //utils/mailer.js
+
+// const nodemailer = require("nodemailer");
+// require("dotenv").config()
+// //nodemailer object
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   auth: {
+//     user: process.env.USER_EMAIL,
+//     pass: process.env.USER_PASSWORD,
+//   },
+// });
+
+// module.exports = transporter;
+
 
 const nodemailer = require("nodemailer");
-require("dotenv").config()
-//nodemailer object
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.USER_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4 
 });
 
 module.exports = transporter;
