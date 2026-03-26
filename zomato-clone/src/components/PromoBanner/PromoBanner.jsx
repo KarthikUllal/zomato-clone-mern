93996924e036adfb9e57c58d648b96183dac4d08
KarthikUllal <karthik2003ku@email.com>
2026-03-26 22:03:06 +0530

    updated promo banner'

--- zomato-clone/src/components/PromoBanner/PromoBanner.jsx
+++ zomato-clone/src/components/PromoBanner/PromoBanner.jsx
@@ -1,14 +1,19 @@
 import "./PromoBanner.css";
+import { useNavigate } from "react-router-dom";
+
+
 
 
 export default function PromoBanner() {
+  const navigate = useNavigate();
+  
   return (
     <section className="promo-section">
       <div className="promo-content">
         <h2>Get up to</h2>
         <h1>50% <span>OFF</span></h1>
         <p>on your dining bills with Zomato</p>
-        <button className="promo-btn">
+        <button className="promo-btn" onClick={() => navigate("/dining")}>
           Check out all the restaurants
         </button>
       </div>
