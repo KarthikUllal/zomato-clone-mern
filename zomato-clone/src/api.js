8aa9086d981b28221aed51724c293577e7d23072
KarthikUllal <karthik2003ku@email.com>
2026-05-02 13:35:07 +0530

    changed local url from production url

--- zomato-clone/src/api.js
+++ zomato-clone/src/api.js
@@ -3,8 +3,8 @@ import axios from "axios"
 //creating an instance of axios with base url
 
 const api = axios.create({
-    // baseURL : import.meta.env.VITE_API_URL || "http://localhost:8000"
-    baseURL : "http://localhost:8000"
+    baseURL : import.meta.env.VITE_API_URL || "http://localhost:8000"
+    // baseURL : "http://localhost:8000"
 })
 
 export default api;
